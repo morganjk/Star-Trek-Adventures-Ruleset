231574dc588b5ac2353756ea622570002cd3f7f7
Wesley Toma-Lee <wesley.r.toma@gmail.com>
2019-09-11 15:43:40 -0400

    Updated graphics code

---
+++ scripts/data_options_core2.lua
@@ -0,0 +1,9 @@
+-- 
+-- Please see the license.html file included with this distribution for 
+-- attribution and copyright information.
+--
+
+function onInit()
+	OptionsManager.registerOption2("DDCL", false, "option_header_game", "option_label_DDCL", "option_entry_cycler", 
+			{ labels = "option_val_DDCL_st", values = "desktopdecal_st", baselabel = "option_val_off", baseval = "off", default = "off" });
+end
